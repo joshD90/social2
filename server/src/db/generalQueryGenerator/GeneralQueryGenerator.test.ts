691a7b96c316dea388344d75049daac74bf67aa0
Joshua Dancey <joshuadancey@hotmail.com>
2023-08-09 23:31:14 +0100

    corrected connection mock, finished testing CreateTableEntryFromPrimitives

--- server/src/db/generalQueryGenerator/GeneralQueryGenerator.test.ts
+++ server/src/db/generalQueryGenerator/GeneralQueryGenerator.test.ts
@@ -1,13 +1,13 @@
 import { Pool } from "mysql2/promise";
 import { GeneralQueryGenerator } from "./GeneralQueryGenerator";
 
-export const connectionMock = jest.fn(() => ({
-  execute: mockExecute,
-  query: mockQuery,
-}));
-
 const mockExecute = jest.fn();
 const mockQuery = jest.fn();
+//as far as this is concerned connection is not a function, treat it like a stub
+export const connectionMock = {
+  execute: mockExecute,
+  query: mockQuery,
+};
 
 describe("General Query Generator Test Suite", () => {
   const sut = new GeneralQueryGenerator(
@@ -18,5 +18,18 @@ describe("General Query Generator Test Suite", () => {
   it("should return the table name for the name getter", () => {
     const actual = sut.getTableName();
     expect(actual).toBe("table1");
+  });
+
+  it("should return the first element of array for CreateTableEntryFromPrimitives if successful", async () => {
+    const someData = { data1: "someData" };
+    mockExecute.mockResolvedValueOnce(["Element 1"]);
+    const actual = await sut.createTableEntryFromPrimitives(someData);
+    expect(actual).toBe("Element 1");
+  });
+  it("should throw an error in CreateTableEntryFromPrimitives if first element is undefined", async () => {
+    const someData = { data1: "someData" };
+    mockExecute.mockResolvedValueOnce([]);
+    const actual = await sut.createTableEntryFromPrimitives(someData);
+    expect(actual).toBeInstanceOf(Error);
   });
 });
