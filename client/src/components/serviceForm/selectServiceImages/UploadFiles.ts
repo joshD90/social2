65c84710d45983d671289cc4471b3644a74c227e
Joshua Dancey <joshuadancey@hotmail.com>
2024-01-19 18:26:18 +0000

    making uploadImages generic for both images and files on frontend and combining these together as well as adding in overloads

---
+++ client/src/components/serviceForm/selectServiceImages/UploadFiles.ts
@@ -0,0 +1,68 @@
+import { SetStateAction } from "react";
+import envIndex from "../../../envIndex/envIndex";
+import isError from "../../../utils/isError/isError";
+import { IFileWithPrimary } from "../../../types/serviceTypes/Service";
+
+function uploadImages(
+  files: IFileWithPrimary[],
+  serviceId: number,
+  setInputErrors: React.Dispatch<SetStateAction<{ [key: string]: string }>>,
+  forImg: true,
+  method: "PUT" | "POST"
+): Promise<boolean>;
+
+function uploadImages(
+  files: File[],
+  serviceId: number,
+  setInputErrors: React.Dispatch<SetStateAction<{ [key: string]: string }>>,
+  forImg: false,
+  method: "PUT" | "POST"
+): Promise<boolean>;
+
+async function uploadImages(
+  files: IFileWithPrimary[] | File[],
+  serviceId: number,
+  setInputErrors: React.Dispatch<SetStateAction<{ [key: string]: string }>>,
+  forImg: boolean,
+  method: "PUT" | "POST"
+) {
+  const formData = new FormData();
+  let url: string;
+  if (forImg) {
+    const mainPicIndex = files.findIndex(
+      (file) => (file as IFileWithPrimary).main_pic
+    );
+    url = `${envIndex.urls.baseUrl}/services/images/${serviceId}`;
+
+    files.forEach((file) => formData.append(`images`, file));
+    formData.append("service_id", serviceId.toString());
+    mainPicIndex && formData.append("mainPicIndex", mainPicIndex.toString());
+  } else {
+    url = `${envIndex.urls.baseUrl}/services/files/${serviceId}`;
+    files.forEach((file) => formData.append("files", file));
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: method,
+      body: formData,
+      credentials: "include",
+    });
+    console.log(response, "response in uploadFiles function");
+    if (!response.ok)
+      throw Error(
+        response.statusText + " for uploading files" + "of type" + forImg
+          ? "image"
+          : "file"
+      );
+
+    return true;
+  } catch (error) {
+    console.log(error, "error in upload images response");
+    if (isError(error)) console.log(error);
+    // setInputErrors((prev) => ({ ...prev, images: (error as Error).message }));
+    return false;
+  }
+}
+
+export default uploadImages;
